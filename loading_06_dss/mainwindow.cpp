20f214449327539dded0864af8d17917d1f3ed03
dengss <xiaozhe@gmail.com>
2022-04-29 18:54:34 +0800

    add replace color to white

--- loading_06_dss/mainwindow.cpp
+++ loading_06_dss/mainwindow.cpp
@@ -15,7 +15,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     m_load06 = new XZ::Wgt::loading_06(this);
     m_load06->setObjectName("loading06");
-    m_load06->setGeometry(100, 100, 100, 100);
+    m_load06->setGeometry(100, 150, 100, 100);
 }
 
 MainWindow::~MainWindow()
@@ -52,4 +52,24 @@ void MainWindow::on_pb_merge_png_clicked()
     img_save->save(sMerge, "png", 80);
     delete img_save;
     delete p_qp_img;
+}
+
+void MainWindow::on_pb_color_replace_clicked()
+{
+    QString sSrc = QString("./animation_merge.png");
+    if( ! QFile::exists(sSrc)) return;
+
+    QImage * img_src = new QImage();
+    img_src->load(sSrc);
+
+    t_u_color_rgb * rgbSrc = reinterpret_cast<t_u_color_rgb *>(img_src->bits());
+    int iCountBits = img_src->width() * img_src->height();
+    while( iCountBits -- > 0 ){
+        rgbSrc->rgba = (rgbSrc->rgba_bits[3] > 0) ? 0xD0FFFFFF : rgbSrc->rgba; // A B G R
+        rgbSrc ++;
+    }
+
+    QString sTar = QString("./animation_merge_white.png");
+    img_src->save(sTar, "png", 80);
+    delete img_src;
 }
--- loading_06_dss/mainwindow.h
+++ loading_06_dss/mainwindow.h
@@ -9,6 +9,12 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+typedef union union_color_rgb
+{
+    uint rgba;
+    uchar rgba_bits[4];
+} t_u_color_rgb;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -22,6 +28,8 @@ protected:
 
 private slots:
     void on_pb_merge_png_clicked();
+
+    void on_pb_color_replace_clicked();
 
 private:
     Ui::MainWindow *ui;
